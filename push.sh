5225a7fade9cdb020addf2c3ba3d8eab5102016d
Bandit Silachai <mbandit001@yahoo.co.th>
2024-06-23 10:24:04 +0700

    📚 update from script: 23-06-2024 10:22:23

---
+++ push.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Get the current date (date time)
+current_datetime=$(date +'%d-%m-%Y %H:%M:%S')
+
+# Check if there are changes to commit
+if [ -n "$(git status --porcelain)" ]; then
+  # Add and commit changes to Git
+  git add .
+  git commit -m "📚 push from script: $current_datetime"
+
+  # Push to GitHub (replace <branch> with your desired branch name)
+  git push origin main
+else
+  echo "No changes to commit."
+fi
