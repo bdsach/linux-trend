aee91725413ddfba065a4f70cbb85f04205cf75c
Bandit Silachai <mbandit001@yahoo.co.th>
2024-07-26 23:50:11 +0700

    📚 update from script: 26-07-2024 23:50:11

--- fetch.js
+++ fetch.js
@@ -47,7 +47,7 @@ const scrap = async (childNum = 2) => {
     waitUntil: "networkidle0",
   });
 
-  let distroCount = 274;
+  let distroCount = 272;
 
   for (let i = 2; i < distroCount; i++) {
     const nameSelector = await page.waitForSelector(
